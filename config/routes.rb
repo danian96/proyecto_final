7c5ed0a9357f46c0aebbd87a935ba4185a53e687
Marcelo <osinaga255@gmail.com>
2018-05-20 22:59:05 -0400

    commit diseno

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
 
->>>>>>> 120f91743a8e006b8baa4ad58b1387cf067dff85
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, only: [:session, :registration], path: 'session',
              path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
