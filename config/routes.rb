bb26112f90db3ad537d47dfc53f9c344d585e41b
alivan <calle.coymbra@gmail.com>
2018-05-21 11:39:10 -0400

    ya

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
   get 'ayuda/index'
-=======
   get 'logs/index'
->>>>>>> 0eb9281b8c53db9e247f79b432b6f1e5b8eaa379
 
   resources :favorites
   resources :assign_details
@@ -13,12 +10,9 @@ Rails.application.routes.draw do
   resources :user_trainings
   resources :trainings
   resources :memorandum_types
-<<<<<<< HEAD
-  
-=======
+
   resources :user_trainings
   resources :trainings
->>>>>>> 0eb9281b8c53db9e247f79b432b6f1e5b8eaa379
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, only: [:session, :registration], path: 'session',
              path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
