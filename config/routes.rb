3a113457bb23ee43d5747284462a9123f76240ea
alivan <calle.coymbra@gmail.com>
2018-05-21 11:22:28 -0400

    ayuda en proceso

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'ayuda/index'
+
   resources :favorites
   resources :assign_details
   resources :assignation_stocks
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
   resources :user_trainings
   resources :trainings
   resources :memorandum_types
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, only: [:session, :registration], path: 'session',
              path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
