78bd5e5660b1a56b37f4b321e9995f91a7670f99
Danilo Mercado Oudalova <danianmercado@gmail.com>
2018-05-21 10:58:47 -0400

    cambios listos

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-<<<<<<< HEAD
   def my_logger
     @@my_logger ||= Logger.new("#{Rails.root}/log/my.log")
   end
@@ -14,7 +13,4 @@ class User < ApplicationRecord
   def before_create
      my_logger.info("Creating user with name #{self.name}")
   end
-=======
-
->>>>>>> 4a1e0d37a451c24b3a9fa61196d49334f9f8e03e
 end
